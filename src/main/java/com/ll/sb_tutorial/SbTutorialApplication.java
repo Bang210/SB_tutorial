1d04248024617d96e567d2484b50f19673ed912a
Bang210 <bangvelope@gmail.com>
2023-11-14 09:38:28 +0900

    start

---
+++ src/main/java/com/ll/sb_tutorial/SbTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sb_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbTutorialApplication.class, args);
+    }
+
+}
